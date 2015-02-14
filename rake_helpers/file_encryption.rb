d1a71ee132832d26a8eb5e64baf535d1a13d4055
Dillon Welch <daw0328@gmail.com>
2015-02-13 17:33:59 -0800

    More updates to rubocop.

--- rake_helpers/file_encryption.rb
+++ rake_helpers/file_encryption.rb
@@ -2,7 +2,7 @@ class FileEncryption
   def self.check
     Output.with_linebreak 'Checking for file-system encryption with FileVault'
     result = `sudo fdesetup status`
-    raise 'Please turn on file-system encryption' if result.match %r(is Off.)
+    fail 'Please turn on file-system encryption' if result.match(/is Off/)
     Output.with_linebreak 'FileVault is on'
   end
 end
